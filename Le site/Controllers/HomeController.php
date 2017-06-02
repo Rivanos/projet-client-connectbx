f04f5d5e4997835865885adfb9cc076479d3561e
Mooch <t.tonneau@gmail.com>
2017-06-02 21:14:34 +0200

    Correction de l'erreur de l'autre débile(moi)

--- Le site/Controllers/HomeController.php
+++ Le site/Controllers/HomeController.php
@@ -6,7 +6,10 @@ class HomeController{ // le nom de votre controller
 	}
 	
 	public function run(){ // la méthode appelée dans index.php
-		
+		/*
+			ICI vient le code php à exécuter avant d'ouvrir la page, 
+			chargement de données, conditions, boucles, ...
+		*/
 		require_once VIEWS . 'home.php'; // affiche la vue (votre page html)
 	}
 	
