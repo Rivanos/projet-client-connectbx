93757bf2fcb6c31d35180038f0ca70286baa69f5
Mesmaeker <mike-mesmaeker@hotmail.com>
2017-06-14 13:54:47 +0200

    Add files via upload

--- Le site/Views/header.php
+++ Le site/Views/header.php
@@ -5,6 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo VIEWS;?>css/style.css">
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 		<script type="text/javascript" src="Views/js/admin.js"></script>
 	</head>
 	<body>
@@ -14,7 +15,7 @@
 					<li><a href="index.php?action=home">Accueil</a></li>
 					<li><a href="index.php?action=admin">Administration</a></li>
 					<!-- Rajouter le lien vers votre page ici -->
-					
+
 					<!-- ex: <li><a href="index.php?action=(le nom que vous voulez)">(Le contenu de la balise a)</a></li> -->
 					<?php if(!empty($_SESSION['authentifie'])){?>
 					<li><a href="index.php?action=logout">Déconnexion</a></li>
