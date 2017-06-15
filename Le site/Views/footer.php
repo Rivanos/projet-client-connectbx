ddd3214938ad43e072b598a1802ed8f1d485da6a
Rivanos <mike-mesmeaker@hotmail.com>
2017-06-15 10:42:00 +0200

    rangement + rajout img

--- Le site/Views/footer.php
+++ Le site/Views/footer.php
@@ -3,10 +3,10 @@
 		<div class="row">
 			<div class="col-md-12">
 			<ul class="socialNetwork">
-			<li class="socialNetworkLI"><a href="3"><img src="img/SocialNetwork/facebook-green.png" class="logoSocialNetwork" alt="logoFacebook" width="35px"/></a>&nbsp;Facebook</li>
-			<li class="socialNetworkLI"><a href="3"><img src="img/SocialNetwork/twitter-green.png" class="logoSocialNetwork" alt="logoTwitter"  width="35px"/></a>&nbsp;Twitter</li>
-			<li class="socialNetworkLI"><a href="3"><img src="img/SocialNetwork/linkedin-green.png" class="logoSocialNetwork" alt="logoLinkedIn"  width="35px"/></a>&nbsp;LinkedIn</li>
-			<li class="socialNetworkLI"><a href="3"><img src="img/SocialNetwork/instagram-green.png" class="logoSocialNetwork" alt="logoInstagram"  width="35px"/></a>&nbsp;Instagram</li>
+			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/facebook-green.png" class="logoSocialNetwork" alt="logoFacebook" width="35px"/></a>&nbsp;Facebook</li>
+			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/twitter-green.png" class="logoSocialNetwork" alt="logoTwitter"  width="35px"/></a>&nbsp;Twitter</li>
+			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/linkedin-green.png" class="logoSocialNetwork" alt="logoLinkedIn"  width="35px"/></a>&nbsp;LinkedIn</li>
+			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/instagram-green.png" class="logoSocialNetwork" alt="logoInstagram"  width="35px"/></a>&nbsp;Instagram</li>
 			</ul>
 		</div>
 	</div>
