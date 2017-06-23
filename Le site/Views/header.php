f70c9cafd7979ea2e2e0ecfb60e2d38d8152b808
Rivanos <mike-mesmeaker@hotmail.com>
2017-06-23 14:36:24 +0200

    push de la vérité

--- Le site/Views/header.php
+++ Le site/Views/header.php
@@ -2,17 +2,45 @@
 <html>
 	<head>
 		<title>ConnectBX</title>
-		<link rel="stylesheet" type="text/css" href="<?php echo VIEWS;?>css/bootstrap/css/bootstrap.min.css?">
+		<link rel="stylesheet" type="text/css" href="<?php echo VIEWS;?>bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo VIEWS;?>css/carte-style.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo VIEWS;?>css/event.css">
 		<link rel="stylesheet" type="text/css" href="<?php echo VIEWS;?>css/style.css">
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <script type="text/javascript" src="<?php echo VIEWS;?>js/script.js"></script>
     <script type="text/javascript" src="<?php echo VIEWS;?>js/admin.js"></script>
+		<script type="text/javascript" src="<?php echo VIEWS;?>js/jquery-2.2.4.js"></script>
+		<script type="text/javascript" src="<?php echo VIEWS;?>bootstrap/js/bootstrap.min.js"></script>
+
 	</head>
 	<body>
 		<header>
-			<nav>
-				<ul>
+			<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">
+            <img alt="logo" src="<?php echo VIEWS;?>Images/logo-01.png" height="30px">
+          </a>
+        </div>
+      </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <form class="navbar-form navbar-left" id="searchNavBar">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
 					<li><a href="index.php?action=home">Accueil</a></li>
 					<li><a href="index.php?action=admin">Administration</a></li>
           <li><a href="index.php?action=contact">Contact</a></li>
@@ -25,5 +53,7 @@
 					<li><a href="index.php?action=logout">Déconnexion</a></li>
 					<?php }?>
 				</ul>
-			</nav>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
 		</header>
--- Le site/Views/home.php
+++ Le site/Views/home.php
@@ -1,8 +1,79 @@
-<h2>Bienvenue sur la page d'accueil</h2>
-<p> Bonjour, le MVC c'est drôle </p>
-
-<?php
-
-
-?>
-
+<div class="jumbotron fond text-center">
+  <h1>La Porte principale des jeunes
+vers le monde associatif.</h1>
+  <select class="commune" name="commune">
+    <option class="disable" value="">Commune</option>
+    <option value="">Anderlecht</option>
+    <option value="">Bruxelles</option>
+    <option value="">Etterbeek</option>
+    <option value="">Jette</option>
+    <option value="">Evere</option>
+    <option value="">Ganshoren</option>
+    <option value="">Ixelles</option>
+    <option value="">Koekelberg</option>
+    <option value="">Auderghem</option>
+    <option value="">Schaerbeek</option>
+    <option value="">Berchem-Sainte-Agathe</option>
+    <option value="">Saint-Gilles</option>
+    <option value="">Molenbeek-end-Saint-Jean</option>
+    <option value="">Saint-Josse-ten-Noode</option>
+    <option value="">Woluwe-Saint-Lambert</option>
+    <option value="">Woluwe-Saint-Pierre</option>
+    <option value="">Uccle</option>
+    <option value="">Forest</option>
+    <option value="">Watermael-Boitsfort</option>
+  </select>
+  <select class="themes" name="themes">
+    <option class="disable" value="">Thématique</option>
+    <option value="">Lorem</option>
+    <option value="">Ipsum</option>
+    <option value="">Lorem</option>
+    <option value="">Ipsum</option>
+    <option value="">Lorem</option>
+  </select>
+  <button type="submit" name="button">Rechercher <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></button>
+</div>
+<div class="container text-center">
+  <div class="col-md-8 col-md-offset-2">
+    <div class="panel">
+      <div class="panel-heading text-center">
+        A propos
+      </div>
+      <div class="panel-body">
+        ConnectBX est, tout simplement, la porte principale des jeunes vers le monde associatif bruxellois. Cartographie, formations, Ambassadeurs, toutes nos actions n’ont qu’un seul objectif : Faciliter les démarches vers l’engagement. S’engager a permis à
+        un tas de jeunes d'acquérir de l'expérience, de se rendre utile, de se construire un réseaux personnel et professionnel, de se découvrir autrement. Autant de raisons pour, au moins, essayer. Un tas d’associations existent qui pourraient parfaitement
+        vous convenir,.. il suffit simplement de les trouver.<br/>
+        <button type="button" name="carte">Carte</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container text-center">
+  <h1>Events</h1>
+  <div class="col-md-4 col-md-offset-1">
+    <div class="jumbotron"></div>
+    <h1>Event1</h1>
+    <p>Description de L'events:<br/>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <div class="col-md-4 col-md-offset-2">
+    <div class="jumbotron"></div>
+    <h1>Event2</h1>
+    <p>Description de L'events:<br/>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</div>
+<div class="container text-center">
+  <div class="col-md-8 col-md-offset-2">
+    <h1>Activités</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</div>
+<div class="container text-center">
+  <div class="col-md-8 col-md-offset-2 padding-top">
+    <h1>Ambassadeurs</h1>
+    <p>4 jeunes activistes représentant chacun une problématique pour lequel ils se sont engagés vont durant toute une année être nos ambassadeurs et vont faire connaître leur travail, ainsi que celui de leur collègues-activistes, au grand public via diverses
+      plateformes et divers moyens.</p>
+  </div>
+</div>
