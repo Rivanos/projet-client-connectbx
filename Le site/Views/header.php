afabcf1e910c9c02d96f09fcb705c3bc59ece8d0
Mooch <t.tonneau@gmail.com>
2017-07-07 10:10:48 +0200

    correction header

--- Le site/Views/header.php
+++ Le site/Views/header.php
@@ -9,11 +9,9 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script type="text/javascript" src="<?php echo VIEWS;?>js/jquery-2.2.4.js"></script>
+		<script type="text/javascript" src="<?php echo VIEWS;?>bootstrap/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="<?php echo VIEWS;?>js/script.js"></script>
 		<script type="text/javascript" src="<?php echo VIEWS;?>js/admin.js"></script>
-        	<script type="text/javascript" src="<?php echo VIEWS;?>js/script.js"></script>
-        	<script type="text/javascript" src="<?php echo VIEWS;?>js/admin.js"></script>
-		<script type="text/javascript" src="<?php echo VIEWS;?>bootstrap/js/bootstrap.min.js"></script>
 		
 	</head>
 	<body>
