5c7dc1506ad2adb0d68de65c335cf9192135115b
GillesYoutou <u2@sheol.HarMa>
2017-08-20 18:31:44 +0200

    refonte page contact

--- Le site/Views/contact.php
+++ Le site/Views/contact.php
@@ -1,64 +1,56 @@
-<section id="contact">
-        <div class="container">
-          <h2>Contact</h2>
-          <div class="col-sm-12">
-            <p><span class="glyphicon glyphicon-map-marker">Bruxelles</span></p>
-            <p><span class="glyphicon glyphicon-phone"><em>A renseigner</em></span></p>
-            <p><span class="glyphicon glyphicon-envelope">info@connectbx.com</span></p>
-          </div>
-          <form class="form-horizontal" action="/action_page.php">
-            <!--Nom et prénoms-->
-            <div class="form-group">
-              <label class="control-label col-sm-3" for="Name">Nom :</label>
-              <div class="col-sm-3">
-                <input type="text" class="form-control" id="nom" placeholder="Enter your name" name="nom">
-              </div>
-              <label class="control-label col-sm-3" for="firstName">Prénom :</label>
-              <div class="col-sm-3">
-                <input type="text" class="form-control" id="prenom" placeholder="Enter your firstname" name="nom">
-              </div>
-            </div>
-            <!--email -->
-            <div class="form-group">
-              <label class="control-label col-sm-3" for="email">Email :</label>
-              <div class="col-sm-9">
-                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-              </div>
-            </div>
-            <!--div class="form-group">
-          <label class="control-label col-sm-2" for="pwd">Password :</label>
-          <div class="col-sm-10">
-            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-          </div>
-        </div-->
-            <!--Multiple select list-->
-            <div class="form-group">
-              <label class="control-label col-sm-3" for="sel1">Sujet de votre message :</label>
-              <div class="col-sm-9">
-                <select class="form-control">
-            <option>Votre avis sur connectBX</option>
-            <option>Potins sur votre conjoint</option>
-            <option>Tintin ou Milou</option>
-            <option>Le bénévolat</option>
-          </select>
-              </div>
-            </div>
-            <!--comments area-->
-
-            <div class="form-group">
-              <label class="control-label col-sm-3" for="comment">Laissez votre avis :</label>
-              <div class="col-sm-9">
-                <textarea class="form-control" id="comment"></textarea>
-              </div>
-            </div>
-            <div class="form-group">
-              <label class="control-label col-sm-3"> Envoyer : </label>
-              <div class="col-sm-3">
-                <button type="submit" class="btn btn-default btn-sm ">
-              <span class="glyphicon glyphicon-envelope"></span>  Submit
-              </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </section>
+<div id ="container">
+  <div class="row">
+    <div class="col-md-2 col-md-offset-2">
+      <h2>CONTACT</h2>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-2 col-md-offset-2">
+      <span>ConnectBX</span>
+      <h6>o</h6>
+    </div>
+    <div class="col-md-5">
+      <p>Dans l'attente de répondre à votre message</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-2 col-md-offset-2">
+      <p>152, rue des Goujons</p>
+      <p>1070 Anderlecht</p>
+      <p> o</p>
+      <p>
+        Tel: +3211000000
+        <br>
+        Fax: +3211000001
+      </p>
+      <p>o</p>
+      <p>Email:<span>info@connectbx.com</span></p>
+      <ul class="socialNetwork">
+			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/facebook-grey.png" class="logoSocialNetwork" alt="logoFacebook" width="26px"/></a></li>
+			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/twitter-grey.png" class="logoSocialNetwork" alt="logoTwitter"  width="26px"/></a></li>
+			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/linkedin-grey.png" class="logoSocialNetwork" alt="logoLinkedIn"  width="26px"/></a></li>
+			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/google+-grey.png" class="logoSocialNetwork" alt="logoGoogle+"  width="26px"/></a></li>
+			</ul>
+    </div>
+    <div class="col-md-6">
+      <form>
+    <div class="form-group">
+      <input type="text" class="form-control" placeholder="Nom" name="Nom" value="">
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" placeholder="Email" name="email" value="">
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" placeholder="Sujet" name="sujet" value="">
+    </div>
+    <div class="form-group">
+      <textarea class="form-control" name="Message" id="comments" rows="5">
+      </textarea>
+    </div>
+    <div class="form-group">
+      <button type="submit" name="envoyer" class="btn">Envoyer</button>
+    </div>
+  </form>
+    </div>
+  </div>
+</div>
