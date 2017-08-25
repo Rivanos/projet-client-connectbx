4a71a7c6c45b35856b2df97f2800cca9023b6b1d
Gary Luypaert <g.luypaert@me.com>
2017-08-25 10:22:16 +0200

    MAJ

--- Le site/Views/footer.php
+++ Le site/Views/footer.php
@@ -5,16 +5,17 @@
 			<ul class="socialNetwork">
 			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/facebook-green.png" class="logoSocialNetwork" alt="logoFacebook" width="35px"/></a>&nbsp;Facebook</li>
 			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/twitter-green.png" class="logoSocialNetwork" alt="logoTwitter"  width="35px"/></a>&nbsp;Twitter</li>
-			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/linkedin-green.png" class="logoSocialNetwork" alt="logoLinkedIn"  width="35px"/></a>&nbsp;LinkedIn</li>
 			<li class="socialNetworkLI"><a href="3"><img src="<?php echo VIEWS;?>Images/SocialNetwork/instagram-green.png" class="logoSocialNetwork" alt="logoInstagram"  width="35px"/></a>&nbsp;Instagram</li>
 			</ul>
 		</div>
 	</div>
 <div class="row">
 	<div class="newsletter">
-		<div id="input-group">
-			<input type="text" class="form-control" placeholder="Votre adresse mail"/>
+		<div class="form-group">
+		<form class="form-inline">
+			<input type="text" class="form-control" placeholder="Votre adresse mail" id="inputNewsletter" />
 			<button type="button" class="btn btn-default" id="btnNewsletter">Inscription</button>
+			</form>
 		</div>
 	</div>
 </div>
@@ -29,5 +30,6 @@
 </div>
 
 </footer>
-</body>
+	<script type="text/javascript" src="<?php echo VIEWS;?>js/script.js"></script>
+	</body>
 </html>
