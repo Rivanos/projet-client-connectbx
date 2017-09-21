3062e89108a675bfe1f3179a53b794cb6e3a5b0e
Gary Luypaert <g.luypaert@me.com>
2017-09-21 09:19:39 +0200

    MAJ

--- Le site/erreur404.php
+++ Le site/erreur404.php
@@ -1,3 +1,18 @@
 
+<section id="page404">
+  <div class="content">
+    <div class="content-inside">
+
+		<div class="container">
+	  		<div id="container-404">
+	  			<img src="Le Site/View/Images/404.png" alt="404" />
+	  		</div>
+		</div>
+
+    </div> <!-- Fermeture content-inside -->
+  </div> <!-- Fermeture content -->
+
+</section>
+
 
 Page erreur 404
