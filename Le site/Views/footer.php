3651dde6b0508397f09e5a98bc15de7771f6dc4d
Mooch <t.tonneau@gmail.com>
2017-09-21 11:15:23 +0200

    correction mineure

--- Le site/Views/footer.php
+++ Le site/Views/footer.php
@@ -23,7 +23,7 @@
 </div>
 <div class="container-fluid">
 	<div class="row">
-		<div id="copyright">
+		<div class="copyright">
 				Copyright © <a href="#">BeCode</a> 2017, tous droits réservés
 		</div>
 	</div>
@@ -32,20 +32,22 @@
 </footer>
 <script type="text/javascript" src="<?php echo VIEWS;?>js/jquery-2.2.4.js"></script>
 <script type="text/javascript" src="<?php echo VIEWS;?>bootstrap/js/bootstrap.min.js"></script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPuNlEkwhQVdbWWZ12PyfUubvQ6ABcClg&callback=initMap">
-</script>
-<script type="text/javascript" src="<?php echo VIEWS;?>js/admin.js"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPuNlEkwhQVdbWWZ12PyfUubvQ6ABcClg&callback=initMap"></script>
 <script type="text/javascript" src="<?php echo VIEWS;?>js/search.js"></script>
-	<script type="text/javascript" src="<?php echo VIEWS;?>js/script.js"></script>
-	<script type="text/javascript" src="<?php echo VIEWS;?>js/affichage_map.js">
-	</script>	
-  <script type="text/javascript" src="<?php echo VIEWS;?>js/header-animate.js"></script>
-	<script src="https://unpkg.com/jquery-aniview@1.0.1/dist/jquery.aniview.js"
+<?php if(isset($_SESSION['logged']) && $_SESSION['logged']){ ?>
+<script type="text/javascript" src="<?php echo VIEWS;?>js/admin.js"></script>
+<?php } else { ?>
+<script type="text/javascript" src="<?php echo VIEWS;?>js/script.js"></script>
+<?php } ?>
+<script type="text/javascript" src="<?php echo VIEWS;?>js/affichage_map.js">
+</script>	
+<script type="text/javascript" src="<?php echo VIEWS;?>js/header-animate.js"></script>
+<script src="https://unpkg.com/jquery-aniview@1.0.1/dist/jquery.aniview.js"
 	integrity="sha384-zDA6q/t525x7f6KD/OaOe24vCxSPU3eraILc2NU+ZA7ISsc3ExQbj8PB56FRMq6H"
 	crossorigin="anonymous"></script>
-	<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
-	<script src="Views/js/animate.js"></script>
-	<script>
+<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+<script src="Views/js/animate.js"></script>
+<script>
 	var config = {
 		easing : 'ease',
 		duration : '800',
@@ -55,6 +57,6 @@
 	window.sr = ScrollReveal();
 	sr.reveal(".event2", config,300);
 	sr.reveal(".event", config, 500);
-	</script>
-	</body>
+</script>
+</body>
 </html>
